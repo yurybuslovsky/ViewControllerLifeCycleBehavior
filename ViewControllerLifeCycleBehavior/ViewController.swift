3207bc1d46e3a06755619213b02a9dfa3e552ac5
Yury Buslovsky <yury.buslovsky@gmail.com>
2020-05-14 16:15:14 +0500

    Initial Commit

---
+++ ViewControllerLifeCycleBehavior/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerLifeCycleBehavior
+//
+//  Created by Yury Buslovsky on 14.05.2020.
+//  Copyright © 2020 Napoleon IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
